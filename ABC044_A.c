3793d3dada11c6ee44417fca89ab85ef811588fb
jh-park23 <parkwilly23gmail.com>
2026-07-16 12:00:00 +0900

    ABC043, ABC044 풀이

---
+++ ABC044_A.c
@@ -0,0 +1,63 @@
+/*Problem Statement
+There is a hotel with the following accommodation fee:
+
+X yen (the currency of Japan) per night, for the first K nights
+Y yen per night, for the (K+1)-th and subsequent nights
+Tak is staying at this hotel for N consecutive nights. Find his total accommodation fee.
+Constraints
+1≤N,K≤10000
+1≤Y<X≤10000
+N,K,X,Y are integers.
+
+Input
+The input is given from Standard Input in the following format:
+N
+K
+X
+Y
+
+Output
+Print Tak's total accommodation fee.
+
+5
+3
+10000
+9000
+
+48000
+
+The accommodation fee is as follows:
+10000 yen for the 
+1-st night
+10000 yen for the 
+2-nd night
+10000 yen for the 
+3-rd night
+9000 yen for the 
+4-th night
+9000 yen for the 
+5-th night
+Thus, the total is 
+48000 yen.
+
+2
+3
+10000
+9000
+
+20000*/
+
+#include<stdio.h>
+int main()
+{
+    int N, K, X, Y, fee=0;
+    scanf("%d %d %d %d", &N, &K, &X, &Y);
+
+    if(N<=K)
+        fee= N*X;
+    else
+        fee= X*K+Y*(N-K);
+
+    printf("%d", fee);
+    return 0;
+}
